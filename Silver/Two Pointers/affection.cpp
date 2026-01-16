69166d766319884523be9ecafb4f940424a8ef36
hannahliu123 <hannah.liu2010@gmail.com>
2026-01-16 14:06:31 -0500

    reflection

--- Silver/Two Pointers/affection.cpp
+++ Silver/Two Pointers/affection.cpp
@@ -10,7 +10,29 @@
 // too. i kinda have a sleep headache rn so imma go to bed but imma be sure to upsolve
 // tmr and like even reimplement it cos i have a ton of time tmrrrr~
 
-//
+// alright, so analyzing my code, i track the indexes of each letter in alp then for each
+// query, i use two pointers to iterate through the array or indexes corresponding to the
+// letter c. We go through that array 2 times (basically one then) so the time complexity
+// is O(Q*max_size_of_array). the max size of an array will be N if the original string
+// is purely one letter. therefore the time complexity is just O(Q*N) which is kinda a
+// stretch because it'll result in a max of 3*10^8 operations
+
+// holy moly bruh. my code is so inefficient omg. i didn't even need to make the alp vector!
+// yeah bro if my code is O(QN) anyways, then why even bother shortening it???? ipasdbral
+// aseirj alksdjrliaeruj alierujliajlajl bruh i could've just had such a clean and simple
+// implementation!!!!! ok let's break down why i never even thought of that tho. for 
+// starters i didn't think O(QN) was possible because that would be 3*10^8 max operations
+// per test while the time limit is only 2 seconds. oh okay so the 10^8 operations per
+// second rule is a conservative estimate. in reality it can fit up to 9*10^8 operations
+// lol. well even if i knew that, i never even considered two pointers on the original
+// vector before i calculated time complexities. i think i was too focused on making my
+// code more efficient.
+
+// lets start from the beginning. the gist of the problem is that you're given a string of
+// letters and a number of operations you can make. your goal is to form the longest
+// line of one letter where u can change a letter per each operation. if you know O(QN) works,
+// for each query, all you need to do is decide which indexes to change within one loop of N.
+// atp i think it's a pretty classic two pointers problem
 
 #include <bits/stdc++.h>
 using namespace std;
