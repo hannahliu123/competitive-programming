2bc8ceb3bc33cbcfb473ca77d760eb28db834d0a
hannahliu123 <hannah.liu2010@gmail.com>
2026-01-15 20:35:01 -0500

    ez difference arrays 😎

---
+++ Silver/Prefix Sums/little_girl.cpp
@@ -0,0 +1,35 @@
+// CF - C. Little Girl and Maximum Sum - R??00
+
+// Start: 8:21
+// End: 8:34        13 mins!!
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+
+    vector<int> a(N);
+    for (auto& i : a) cin >> i;
+    sort(a.begin(), a.end());
+    vector<int> diff(N+1, 0);
+
+    while (Q--) {
+        int l, r;
+        cin >> l >> r;
+        l--; r;
+        diff[l]++; diff[r]--;
+    }
+
+    for (int i{1}; i <= N; ++i) {
+        diff[i] += diff[i-1];
+    } sort(diff.begin(), diff.end());
+
+    long long ans = 0LL;
+    for (int i{0}; i < N; ++i) {
+        ans += (long long)diff[i+1]*a[i];
+    }
+
+    cout << ans << endl;
+}
