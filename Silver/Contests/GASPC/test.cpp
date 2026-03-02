a5ea42fe0da0810e881eec6ff7cb0f565c5b8f2d
hannahliu123 <hannah.liu2010@gmail.com>
2026-03-02 16:45:03 -0500

    decimals pmo

--- Silver/Contests/GASPC/test.cpp
+++ Silver/Contests/GASPC/test.cpp
@@ -2,18 +2,8 @@
 using namespace std;
 
 int main() {
-    char c; cin >> c;
-    string trash; getline(cin, trash);
-    string s;
-    getline(cin, s);
-    char c2;
-    if (c <= 90) c2 = c+32;
-    else c2 = c-32;
-    
-    int cnt = 0;
-    for (int i{0}; i < (int)s.size(); i++) {
-        if (s[i] == c || s[i] == c2) cnt++;
-    }
-    cout << cnt;
-    return 0;
+    double val = 0.45;
+    printf("%.1f", val);
+    cout << int(val * 100) << '\n';
+    cout << fixed << setprecision(1) << val << endl;
 }
