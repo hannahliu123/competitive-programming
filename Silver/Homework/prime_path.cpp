7db061d16f0985dfbac217792dc1eb709678611c
hannahliu123 <hannah.liu2010@gmail.com>
2026-03-05 21:27:03 -0500

    move locations

---
+++ Silver/Homework/prime_path.cpp
@@ -0,0 +1,14 @@
+// H - Prime Path
+
+// Start: 6:40
+// End: 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n1, n2; cin >> n1 >> n2;
+    }
+}
---
+++ Silver/Homework/tree.cpp
@@ -0,0 +1,11 @@
+// G - Tree
+
+// Start: 6:20
+// End: 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ;
+}
