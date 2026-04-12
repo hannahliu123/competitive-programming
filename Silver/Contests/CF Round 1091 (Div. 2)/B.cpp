1486f95cabc938b263f6f8077c2b584441915184
hannahliu123 <hannah.liu2010@gmail.com>
2026-04-12 11:18:44 -0400

    upsolve flip bit

--- Silver/Contests/CF Round 1091 (Div. 2)/B.cpp
+++ Silver/Contests/CF Round 1091 (Div. 2)/B.cpp
@@ -7,27 +7,51 @@ int main() {
     int t; cin >> t;
     while (t--) {
         int N, K; cin >> N >> K;
-        vector<int> a(N);
-        for (auto& i : a) cin >> i;
+        // vector<int> a(N);
+        // for (auto& i : a) cin >> i;
+        // int k; cin >> k;
+
+        // int ans = 0, x = a[--k];
+        // bool flipped = false;
+        // int l = 0, r = N-1;
+        // while (true) {
+        //     if (flipped) {
+        //         while (l<k && a[l]!=x) l++;
+        //         while (r>k && a[r]!=x) r--;
+        //     } else {
+        //         while (l<k && a[l]==x) l++;
+        //         while (r>k && a[r]==x) r--;
+        //     }
+
+        //     if (l==r && !flipped) break;
+
+        //     flipped = (flipped? false : true);
+        //     ans++;
+        // }
+
+        // i boundary is when a[i] != a[i+1]. we add padding on both sides of array a to make
+        // sure the first and last values are counted as a boundary if they aren't equal to 
+        // x
+        // treat each "boundary" as smthing you want to remove. It's kinda similar to if you
+        // deleted all repetitions of the same value in a row (ex. 100011 -> 101). say you 
+        // choose to flip a region [l, r]. if l-1 or ris a boundary, it is removed. we can
+        // remove at most two boundaries per 1 flip operation (one on each side of k). because
+        // l <= k and r >= k, the boundaries to the left of k (l) must have an index <k (bc
+        // l<=k so the boundary that is removed is <k). for the boundaries to the right of k (r),
+        // it must have an index >= k (cos if u flip r, the boundary at r disappears)
+        vector<int> a(N+2);
+        for (int i=1; i <= N; i++) cin >> a[i];
         int k; cin >> k;
-
-        int ans = 0, x = a[--k];
-        bool flipped = false;
-        int l = 0, r = N-1;
-        while (true) {
-            if (flipped) {
-                while (l<k && a[l]!=x) l++;
-                while (r>k && a[r]!=x) r--;
-            } else {
-                while (l<k && a[l]==x) l++;
-                while (r>k && a[r]==x) r--;
+        int x = a[k];
+        a[0] = x; a[N+1] = x;
+        int left=0, right=0;
+        for (int i{0}; i < N+1; i++) {
+            if (a[i]!=a[i+1]) {
+                if (i < k) left++;
+                else right++;
             }
-
-            if (l==r && !flipped) break;
-
-            flipped = (flipped? false : true);
-            ans++;
         }
+        int ans = max(left, right);
 
         cout << ans << '\n';
     }
--- Silver/Contests/CF Round 1091 (Div. 2)/D.cpp
+++ Silver/Contests/CF Round 1091 (Div. 2)/D.cpp
@@ -1,5 +1,17 @@
 // D
 
+// split this up into regions between each special index. no matter what, if you have two
+// boundaries anywhere but in different regions, that means theres at least 1 special index
+// between them so they can be paired and flipped together to get rid of them. thinking of
+// this problem as boundaries is actually so so helpful because the order in which you choose
+// to flip/pair these boundaries doesnt matter, as long as you get rid of them. it's like
+// combining areas of the same value until everything is the same. so, the answer will always
+// be total_boundaries/2 because you can pair all of them UNLESS there is a region with more
+// boundaries than total_boundaries/2. in that case, you can only pair some of them with
+// boundaries outside that region. the remaining boundaries will need to be removed one by
+// one by pairing it with the nearest special index. these flip operations will only get rid
+// of one boundary per flip.
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,14 +19,25 @@ int main() {
     int t; cin >> t;
     while (t--) {
         int N, K; cin >> N >> K;
-        vector<int> a(N);
-        for (auto& i : a) cin >> i;
-        vector<int> p(K);
-        for (auto& i : p) cin >> i;
+        vector<int> a(N+2);
+        for (int i=1; i <= N; i++) cin >> a[i];
+        vector<int> p(K+2);
+        for (int i=1; i <= K; i++) cin >> p[i];
+        int x = a[p[1]];
+        a[0] = x; a[N+1] = x;
+        p[0] = 0; p[K+1] = N+1;
 
-        int ans = 0, x = a[p[0]];
-        ;
+        int mx = 0;     // max number of boundaries in a region
+        int tot = 0;    // total number of boundaries
+        for (int k=1; k <= K+1; k++) {
+            int curr = 0;   // number of boundaries from prev special index to curr special index-1
+            for (int i=p[k-1]; i < p[k]; i++) {
+                if (a[i] != a[i+1]) curr++;
+            }
+            mx = max(mx, curr);
+            tot += curr;
+        }
 
-        cout << ans << '\n';
+        cout << max(mx, tot/2) << '\n';
     }
 }
