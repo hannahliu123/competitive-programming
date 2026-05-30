21d8a2be3e0b790c0d1b2f0cd703a41892a7d31b
hannahliu123 <hannah.liu2010@gmail.com>
2026-05-30 12:45:44 -0400

    add & mult with bitwise

--- Silver/Bitwise Operations/addition_multiplication.cpp
+++ Silver/Bitwise Operations/addition_multiplication.cpp
@@ -6,9 +6,23 @@ using namespace std;
 // Addition of 2 integers using bit operations
 int add(int a, int b) {
     while (b > 0) {
-        ;
-    }
-    return ;
+		int carry = a & b;
+		a ^= b;     // positions w/o carry (01 or 10)
+		b = carry << 1;     // b holds the value we want to carry over
+	}
+	return a;
+}
+
+int mult(int a, int b) {
+	int c = 0;
+	while (b > 0) { // keep adding a's value once or twice
+		if ((b & 1) == 1) { // add a once (otherwise if 0, skip)
+			c = add(c, a);  // Use the addition function we coded previously
+		}
+		a <<= 1;    // multiply by two for the next iteration
+		b >>= 1;    // get rid of 0th bit (already processed)
+	}
+	return c;
 }
 
 int main() {
@@ -16,4 +30,5 @@ int main() {
     cin >> a >> b;
 
     cout << add(a, b) << endl;
+    cout << mult(a, b) << endl;
 }
