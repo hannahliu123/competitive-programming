d6cfa46c17e861e5a0413142439bbeade17185c6
Hannah Liu <hannah.liu2010@gmail.com>
2026-06-12 15:32:08 -0400

    YESSS FINALLY WOOHOOO

--- Silver/Random Practice/adjust_the_presentation.cpp
+++ Silver/Random Practice/adjust_the_presentation.cpp
@@ -1,4 +1,4 @@
-// CF - C2. Adjust The Presentation (Hard Version) - R1x00
+// CF - C2. Adjust The Presentation (Hard Version) - R1900
 
 // ok so my implementation for this problem got really really messy but the idea is
 // there. essentially, for each query, i want to see if the first occurances for each
@@ -15,15 +15,22 @@
 // should've just used a set instead of a priority queue because i can insert and erase
 // items
 
+// holy crap okay so i finally got it and the reason i got wa was because i didn't really
+// understand what my code was doing and i was too lazy to actually debug through a test
+// case. so apparently for each query, what i was doing was checking the previous member
+// that was suppoused to present the slide and the current member that will be presenting
+// that slide and then seeing if there were any problems around those two members that were
+// either created or fixed by the update. however, if those members are next to eachother
+// in a (presentation order), then the problems they fix/create would be double counted
+// whereas if they weren't next to eachother, their problems would only be counted once.
+// bro tspmo but now im trying to remember what my old code did. imma try reimplementing 
+// it that way because i still dont understand why it doesn't work.
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int N, M, Q;
-vector<int> a;
-vector<int> b;
-vector<int> idx;
 vector<set<int>> occ;
-// vector<pair<bool,bool>> good;
 
 int getFirstOcc(int member) {
     if (occ[member].empty()) return M;
@@ -34,26 +41,23 @@ int main() {
     int t; cin >> t;
     while (t--) {
         cin >> N >> M >> Q;
-        a.clear(); a.resize(N);
-        idx.clear(); idx.resize(N);
-        for (int i=0; i < N; i++) {     // order of members
+        vector<int> a(N); // presenting order
+        vector<int> idx(N); // each member's order in a
+        for (int i=0; i < N; i++) {
             cin >> a[i]; a[i]--;
             idx[a[i]] = i;
-        } b.clear(); b.resize(M);
+        } vector<int> b(M);
         occ.clear(); occ.resize(N, set<int>{}); // slides each member must present
         for (int i=0; i < M; i++) {
             cin >> b[i]; b[i]--;    // member presenting slide i
             occ[b[i]].insert(i);
         }
 
-        // good.clear(); good.resize(N, {true,true});
         int problems = 0;
         for (int i=0; i < N-1; i++) {
             int firstOcc = getFirstOcc(a[i]);
             int firstOccRight = getFirstOcc(a[i+1]);
             if (firstOcc > firstOccRight) {
-                // good[a[i]].second = false;
-                // good[a[i+1]].first = false;
                 problems += 1;
             }
         }
@@ -66,22 +70,33 @@ int main() {
             slide--; member--;
 
             int prevMember = b[slide];
-            int prevIdx = idx[prevMember];
+            int prevIdx = idx[prevMember];  // index in a (presenting order)
             int currIdx = idx[member];
-            int firstOcc = getFirstOcc(a[prevIdx]);
-            int currOcc = getFirstOcc(a[currIdx]);
 
-            b[slide] = member;
-            occ[prevMember].erase(slide);
-            occ[member].insert(slide);
-
+            int firstOcc = getFirstOcc(a[prevIdx]); // first occurance of slide's previous member before it's removed from this slide
             if (prevIdx > 0) {
                 int firstOccLeft = getFirstOcc(a[prevIdx-1]);
                 problems -= firstOcc < firstOccLeft;
             } if (prevIdx < N-1) {
                 int firstOccRight = getFirstOcc(a[prevIdx+1]);
                 problems -= firstOcc > firstOccRight;
-            } firstOcc = getFirstOcc(a[prevIdx]);
+            }
+
+            int currOcc = getFirstOcc(a[currIdx]);  // first occurance of slide's current member before it's assigned to this slide
+            if (currIdx > 0) {
+                int firstOccLeft = getFirstOcc(a[currIdx-1]);
+                if (a[currIdx-1] != prevMember) problems -= currOcc < firstOccLeft;
+            } if (currIdx < N-1) {
+                int firstOccRight = getFirstOcc(a[currIdx+1]);
+                if (a[currIdx+1] != prevMember) problems -= currOcc > firstOccRight;
+            }
+
+            // assign this slide to member
+            b[slide] = member;
+            occ[prevMember].erase(slide);
+            occ[member].insert(slide);
+
+            firstOcc = getFirstOcc(a[prevIdx]); // update prevMember's first slide
             if (prevIdx > 0) {
                 int firstOccLeft = getFirstOcc(a[prevIdx-1]);
                 problems += firstOcc < firstOccLeft;
@@ -90,73 +105,15 @@ int main() {
                 problems += firstOcc > firstOccRight;
             } 
             
+            currOcc = getFirstOcc(a[currIdx]);  // update currMember's first slide
             if (currIdx > 0) {
                 int firstOccLeft = getFirstOcc(a[currIdx-1]);
-                problems -= currOcc < firstOccLeft;
+                if (a[currIdx-1] != prevMember) problems += currOcc < firstOccLeft;
             } if (currIdx < N-1) {
                 int firstOccRight = getFirstOcc(a[currIdx+1]);
-                problems -= currOcc > firstOccRight;
-            } currOcc = getFirstOcc(a[currIdx]);
-            if (currIdx > 0) {
-                int firstOccLeft = getFirstOcc(a[currIdx-1]);
-                problems += currOcc < firstOccLeft;
-            } if (currIdx < N-1) {
-                int firstOccRight = getFirstOcc(a[currIdx+1]);
-                problems += currOcc > firstOccRight;
+                if (a[currIdx+1] != prevMember) problems += currOcc > firstOccRight;
             }
 
-            // // check if the member that got removed creates any problems
-            // int firstOcc = getFirstOcc(a[prevIdx]);
-            // if (prevIdx > 0) {
-            //     int firstOccLeft = getFirstOcc(a[prevIdx-1]);
-            //     if (good[a[prevIdx]].first && firstOcc < firstOccLeft) {
-            //         // good[a[prevIdx]].first = false;
-            //         // good[a[prevIdx-1]].second = false;
-            //         problems += 1;
-            //     } else if (!good[a[prevIdx]].first && firstOcc > firstOccLeft) {
-            //         // good[a[prevIdx]].first = true;
-            //         // good[a[prevIdx-1]].second = true;
-            //         problems -= 2;
-            //     }
-            // } if (prevIdx < N-1) {
-            //     int firstOccRight = getFirstOcc(a[prevIdx+1]);
-            //     if (good[a[prevIdx]].second && firstOcc > firstOccRight) {
-            //         // good[a[prevIdx]].second = false;
-            //         // good[a[prevIdx+1]].first = false;
-            //         problems += 2;
-            //     } else if (!good[a[prevIdx]].second && firstOcc < firstOccRight) {
-            //         // good[a[prevIdx]].second = true;
-            //         // good[a[prevIdx+1]].first = true;
-            //         problems -= 2;
-            //     } 
-            // }
-
-            // // check if the member that got added creates any problems
-            // int currOcc = getFirstOcc(a[currIdx]);
-            // if (currIdx > 0) {
-            //     int firstOccLeft = getFirstOcc(a[currIdx-1]);
-            //     if (good[a[currIdx]].first && currOcc < firstOccLeft) {
-            //         // good[a[currIdx]].first = false;
-            //         // good[a[currIdx-1]].second = false;
-            //         problems += 2;
-            //     } else if (!good[a[currIdx]].first && currOcc > firstOccLeft) {
-            //         // good[a[currIdx]].first = true;
-            //         // good[a[currIdx-1]].second = true;
-            //         problems -= 2;
-            //     }
-            // } if (currIdx < N-1) {
-            //     int firstOccRight = getFirstOcc(a[currIdx+1]);
-            //     if (good[a[currIdx]].second && currOcc > firstOccRight) {
-            //         // good[a[currIdx]].second = false;
-            //         // good[a[currIdx+1]].first = false;
-            //         problems += 2;
-            //     } else if (!good[a[currIdx]].second && currOcc < firstOccRight) {
-            //         // good[a[currIdx]].second = true;
-            //         // good[a[currIdx+1]].first = true;
-            //         problems -= 2;
-            //     } 
-            // }
-            
             if (problems > 0) cout << "TIDAK\n";
             else cout << "YA\n";
         }
