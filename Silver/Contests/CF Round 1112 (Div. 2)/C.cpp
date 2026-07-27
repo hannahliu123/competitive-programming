e76ddcd5c7837a9597ed853d1a8c31bb473eb1d6
Hannah Liu <hannah.liu2010@gmail.com>
2026-07-27 13:04:05 -0400

    bruhhh upsolve c

--- Silver/Contests/CF Round 1112 (Div. 2)/C.cpp
+++ Silver/Contests/CF Round 1112 (Div. 2)/C.cpp
@@ -1,5 +1,16 @@
 // C
 
+// bro idk how tf i didn't realize this but i literally know i could solve this if i
+// could just find a way to check if a sequence of length x was possible. bruh im so
+// pissed. if i iterate over every possible length then try to find a sequence of that
+// length it's an O(N^2) solution. it's important to realize here that binary serach is
+// NOT an option because we need a fixed length in order to find the solution so we can
+// greedily chose each item. if we know the length then we know the left rank and right
+// rank as we're building our sequence from left to right. if we did binary serach, just
+// because of length of 5 works doesn't mean a length of 3 will work. If we checked if a
+// lenght of x or more was possible, then length is no longer fixed so its not a monotonic
+// function. UGFHHHHH
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -18,32 +29,21 @@ int main() {
             cin >> l[i] >> r[i] >> u[i] >> v[i];
         }
 
-        int nxt = 1;
-        priority_queue<pair<int,int>> pq{}; // i, j
-        for (int i=0; i < N; i++) {
-            if (nxt<l[i] || nxt>r[i]) {
-                pq.push({i,nxt});
-                nxt++;
-            }
-        }
-
-        nxt = 1;
-        while (!pq.empty()) {
-            int idx = pq.top().first;
-            int j = pq.top().second;
-            pq.pop();
-
-            if (nxt>=u[idx] && nxt<=v[idx]) {   // invalid!
-                for (int i=idx+1; i < N; i++) {
-                    if (j<l[i] || j>r[i]) {
-                        pq.push({i,j});
-                        j++;
-                    }
+        int ans = 0;
+        for (int len=1; len <= N; len++) {
+            // check if a sequence of this len is possible
+            int left=1, right=len;
+            bool pos = false;
+            for (int i=0; i < N; i++) {
+                if ((left<l[i] || left>r[i]) && (right<u[i] || right>v[i])) {
+                    left++; right--;
+                } if (left==len+1 && right==0) {
+                    pos = true;
+                    break;
                 }
-                nxt = 1;
-            } else nxt++;
+            } if (pos) ans = max(ans, len);
         }
         
-        cout << nxt-1 << '\n';
+        cout << ans << '\n';
     }
 }
