d948252f0f878c2c8fa5d83d5bc0d3e4ed7b84ac
Hannah Liu <hannah.liu2010@gmail.com>
2026-08-27 08:42:37 -0400

    ohhhhhh oopsie daisies

--- Silver/Random Practice/mixing_mexes.cpp
+++ Silver/Random Practice/mixing_mexes.cpp
@@ -1,7 +1,11 @@
 // G. Mixing MEXes - R1800
 
-// Start: 8:26-8:35
-// End: 
+// this took like 1 hr and 10 mins but the idea was pretty intuitive for me of just
+// finding contributions. implementation wasnt great and finding the specifics took
+// a while. if i just made my code longer and used more loops i think it wouldve been
+// a bit cleaner but the code makes sense to me. i did get tle bc i initialized the
+// icc loop for every test case which wasnt a great idea heh. ive never hahd an issue
+// with that b4 tho so im glad i encountered it
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -9,14 +13,14 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int t; cin >> t;
+    vector<pair<int,int>> occ(1000001, {0,t});
     while (t--) {
         int N; cin >> N;
 
         int lsum = 0;
         vector<int> l(N);
-        vector<int> occ(1000001, 0);
         vector<pair<int,int>> def(N, {INT_MAX, INT_MAX});   // default mex, mex after gaining default mex
         vector<vector<int>> a(N);
         for (int i=0; i < N; i++) {
@@ -25,7 +29,8 @@ int main() {
             vector<int> ai(l[i]);
             for (int j=0; j < l[i]; j++) {
                 cin >> ai[j];
-                occ[ai[j]]++;
+                if (occ[ai[j]].second > t) occ[ai[j]]={0,t};
+                occ[ai[j]].first++;
             } sort(ai.begin(), ai.end());
             int mex = 0, mex2 = -1;
             for (int j=0; j < l[i]; j++) {
@@ -53,7 +58,8 @@ int main() {
         for (int i=0; i < N; i++) {
             long long sum_less = 0;
             int cnt_less = 0;
-            int x = occ[def[i].first];
+            int x = 0;
+            if (occ[def[i].first].second==t) x=occ[def[i].first].first;
             for (int j=0; j < l[i]; j++) {
                 if (a[i][j] == def[i].first) x--;
                 if (a[i][j] < def[i].first && ((j==0 || a[i][j] != a[i][j-1]) && (j==l[i]-1 || a[i][j] != a[i][j+1]))) {
