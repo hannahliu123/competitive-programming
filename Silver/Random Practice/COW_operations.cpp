c2388f4fda26ba3906c82147be2c66480dda6b9f
Hannah Liu <hannah.liu2010@gmail.com>
2026-09-07 16:26:01 -0400

    upsolve greedy

--- Silver/Random Practice/COW_operations.cpp
+++ Silver/Random Practice/COW_operations.cpp
@@ -13,7 +13,24 @@
 // the original array itself. knowing this, you can just precompute all 4 possible arrays
 // and get the answer in O(1) time for each query (technically O(4) lol)
 
-// um so the editorial solution is actually very different
+// um so the editorial solution is actually very different. they key was to look for
+// invariants, or aspects of the problem that never change. in this case, after combing
+// through a few test cases, i mightve been able to realize parity could've been an
+// invariant (it's pretty common so i should keep that in mind from now on). also, i
+// could've noticed that we can always apply operations in a way that allow us to swap
+// two adjacent letters. as a result, we can take any string and keep swapping letters
+// until we just have C...O...W... or something like that. THIS is the reason why the
+// order of operations doesn't matter!!!
+
+// okay so now that we know we can rearrange s, it's obviously optimal for us to
+// shorten it as much as possible, so we can reduce it down to 0/1 Cs, 0/1 Os, and 0/1
+// Ws. The possible combinations can be represened by these (such that index 0 is the
+// cnt of Cs, 1 is Os, and 2 is Ws):
+// {1,0,0}   {0,1,0}   {0,0,1}   {1,1,0}   {1,0,1}   {0,1,1}   {1,1,1}
+
+// All strings can be reduced down to one of those 7 patterns. the 1st is obviously
+// fine because that's just a C. The only other option that works is the 6th. you could
+// compute the number of each letter using prefix sums then just find the parities heh
 
 #include <bits/stdc++.h>
 using namespace std;
