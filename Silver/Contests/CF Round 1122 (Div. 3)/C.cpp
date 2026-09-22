59e88a1e660b83c2ad57e13b189edd74d1cc1756
Hannah Liu <hannah.liu2010@gmail.com>
2026-09-22 10:00:41 -0400

    c reflection

--- Silver/Contests/CF Round 1122 (Div. 3)/C.cpp
+++ Silver/Contests/CF Round 1122 (Div. 3)/C.cpp
@@ -1,3 +1,12 @@
+// this problem took my like 20 minutes which ig is okay and im not mad about but i
+// feel like it's so simple i def could've gotten it quicker. I mean you jsut need to
+// greedily realize you can make any number after you've seen a 0 or 1 into a 0 or 1
+// of your choice then you can just test every boundary value. then notice how if you
+// start with a chain of 0s or 1s you need to keep it but the 0s dont actually matter
+// because a more optimal solution would've taken place later.
+
+// ig indexing could've been a bit confusing so i'll take 20 mins
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -19,7 +28,6 @@ int main() {
         if (s[0]=='1') {
             cout << suff[0] << '\n';
         } else {
-            bool seen1=false;
             int ans = INT_MAX;
             for (int i=0; i <= N; i++) {
                 ans = min(ans, pref[i]+suff[i]);
